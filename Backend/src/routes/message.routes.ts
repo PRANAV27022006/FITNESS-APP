515a73f45b15bafb214b5833e5ee3d9b9137c60b
pranav27022006 <ps2315001624@gmail.com>
2026-02-13 11:33:46 +0530

    Removed Backend folder

--- Backend/src/routes/message.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { getMessages, sendMessage } from "../controllers/message.controller";
-import { authMiddlware } from "../middlewares/auth.middleware";
-
-export const messageRouter = Router();
-
-messageRouter.post('/send',authMiddlware,sendMessage);
-messageRouter.post('/get',authMiddlware,getMessages)
